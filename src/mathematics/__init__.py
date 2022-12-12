391ccee02aa69e7ba14c8c6d9808ac3d9287a53d
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2022-12-11 21:54:02 -0800

    mathematics names

---
+++ src/mathematics/__init__.py
@@ -0,0 +1,69 @@
+'''
+usefulpy-mathematics
+
+A library to contain 
+
+RELEASE NOTES:
+0
+ 0.0
+  Version 0.0.0:
+   mathematics.py contains many mathematical functions.
+  Version 0.0.1:
+   An updated description and various bug fixes. Cleaner looking code with more
+   comments.
+  Version 0.0.2:
+   Some small bug fixes
+   Raises warnings at unfinished sections
+ 0.1
+  Version 0.1.0
+   Separated into sections and placed into a folder of its own
+  Version 0.1.1
+   Mostly some small bugfixes and clearer commenting throughout
+  Version 0.1.2
+   Several bugfixes, some work on algebraic solver and improvements on eq
+  Version 0.1.3
+   Heavy improvements in nmath, small bugfixes throughout
+  Version 0.1.4
+   Small improvement throughout.
+ 0.2
+  Version 0.2.0
+   Heavy bugfixing throughout. Deprecated algebraic solver and eq
+  Version 0.2.1
+   More bugfixing, some more functions. Efficiency increased
+1
+ 1.0
+  Version 1.0.0:
+   Entirety of folder restructured. Much improved use of mathfuncs. Expression
+   check has taken the place of eq and algebraic solver. Basic CAS implemented
+   for the new mathfunc-eq-solver merge. Greater efficiency and power to most
+   other areas. nmath made much smaller, most of its functionality has been
+   moved to the mathfunc file.
+  Version 1.0.1:
+   Heavy bugfixing
+ 1.1
+  Version 1.1.0:
+   Remade CAS system. Moved things around to nmath.
+2
+ 2.0
+  Version 2.0.0:
+   Seperated from main Usefulpython system.
+'''
+# CAS engine
+# - sets
+# - Display
+# - GUI Box for input?
+# - Geometry
+# -- Display
+# -- Solver?
+# - Numeric Types
+# -- Basenum
+# -- Quaternion
+# -- Unit Type (enums?)
+# -- Logic and set based definition of type? Quaternions = R(i, j, k), Matrix = R(m*n)?
+# -- Boolean?
+# -- Vector, Matrix, Tensor?
+# -- Tensor-based systems of equations?
+# Constants?
+
+
+# C/C++?
