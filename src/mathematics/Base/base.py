40cb556b408686c835e0b212804ca01f2f5f4507
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2022-12-16 21:37:17 -0800

    skeleton base class

---
+++ src/mathematics/Base/base.py
@@ -0,0 +1,52 @@
+'''
+Base class for all CAS classes
+'''
+
+from abc import abstractmethod
+import types
+from . import rules
+
+
+class base:
+    exact: bool
+    value: int
+    subrules: tuple
+    var: tuple
+    fn: set
+    evaluation: types.FunctionType
+    system: types.FunctionType
+    classRules: rules.rule
+
+    def __repr__(self, /):
+        return f'CAS[{repr(str(self))}]'
+    
+    @abstractmethod
+    def __str__(self, /)->str:
+        pass
+    
+    @abstractmethod
+    def tex(self, /)->str:
+        pass
+
+    @abstractmethod
+    def substitute(self, data: dict, /):
+        pass
+
+    def applyRules(self, /):
+        for rule in self.subrules:
+            test = rule.apply(self)
+            if test:
+                return test
+        test = self.classRules.apply(self)
+        if test:
+            return test
+        for rule in self.system().globalrules:
+            test = rule.apply(self)
+            if test:
+                return test
+        return self
+
+    def evalstr(self, /)->str:
+        if self.exact:
+            return str(self.value)
+        return str(self)
---
+++ src/mathematics/Base/rules.py
@@ -0,0 +1,7 @@
+import types
+
+# 
+
+class rule:
+    def __init__(self, apply):
+        self.apply = apply
