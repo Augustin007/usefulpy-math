750fc554a48cfa6df0b4c401de6ac379d10df2ee
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2022-12-19 22:15:12 -0800

    Beginning to outline rules

--- src/mathematics/Algebra/add.py
+++ src/mathematics/Algebra/add.py
@@ -1,8 +1,14 @@
 from expression import commutative
 
 class add(commutative):
+    def __new__(cls, name, *args):
+        self = tuple.__new__(cls, name, args)
+        # subrules = cls
     def __str__(self, /)->str:
         return '+'.join(map(str, self)).replace('+-', '-')
     
     def tex(self, /)->str:
+        pass
+
+    def substitute(self, data: dict, /):
         pass
--- src/mathematics/Algebra/expression.py
+++ src/mathematics/Algebra/expression.py
@@ -1,8 +1,41 @@
 from ..Base.base import base, _arithmetic
+from ..Base.rules import rule
 
 class expression(base, _arithmetic):
     pass
 
+@rule
+def assosiative_property(self):
+    if not hasattr(self, '__iter__'):
+        return self
+    selftype: type = type(self)
+    new = []
+    for term in self:
+        if type(term) == selftype:
+            new.extend(term)
+            continue
+        new.append(term)
+    if new == tuple(self):
+        return self
+    return selftype(new)
+
+@rule
+def commutative_property(self):
+    return self
+
+ 
+@rule
+def distribute(self):
+    return NotImplemented
+
+@rule
+def combine(self):
+    if not hasattr(self, 'superoper'):
+        return self
+    superoper = self.superoper
+    for term in self:
+        pass
+    return NotImplemented
 class commutative(expression, tuple):
     pass
 
