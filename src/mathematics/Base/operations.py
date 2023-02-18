1016dd40a1e7692b24034859fdd6a760bcc116e1
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2023-02-18 12:26:10 -0800

    recovering base syntax from old future/Base/base

--- src/mathematics/Base/operations.py
+++ src/mathematics/Base/operations.py
@@ -1,5 +1,5 @@
-from ..Base.base import symbol, CAS
-from ..Base.rule import pattern
+from .base import symbol, CAS
+from .rule import pattern
 from collections import OrderedDict
 
 exists = symbol('exists', OrderedDict(a=0), fstring =  '∃%s', latex_fstring = '\\exists{%s}')
@@ -17,7 +17,11 @@ zfcset = symbol('zfcset', OrderedDict(a=2))
 # dual_numbers = number.create('dual numbers', 'a+bε').a_in(R).b_in(R).ε_ni('ε**2==0 & ε!=0')
 #other sets
 #type theory? zfcset
-# equals?
-# move operations to base
 
+commutative = pattern((symbol['A', 'B'], symbol['B', 'A']))
+symmetric = pattern((exists('A'), symbol['A', 'A']))
+transitive = pattern((land(symbol['A', 'B'], symbol['B', 'C']), symbol['A', 'C']))
+associative = pattern((symbol['A', symbol['B', 'C']], symbol[symbol['A', 'B'], 'C']))
 
+equality = symbol('equals',OrderedDict(a=0,b=0), fstring = '%s=%s', latex_fstring='{%s}={%s}', rules = (commutative, symmetric, transitive))
+addition = symbol('addition', OrderedDict(terms=1), fstring = '{chr(43).join(self.terms)}', latex_fstring = '{chr(43).join(self.terms)}', rules = (commutative, associative))
